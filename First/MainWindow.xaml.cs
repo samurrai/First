0c3c0f5923f102e36a8102014a8611b3126f5ceb
samurrai <samurrrai.r@gmail.com>
2019-10-03 23:39:33 +0600

    initial

--- First/MainWindow.xaml.cs
+++ First/MainWindow.xaml.cs
@@ -19,61 +19,61 @@ namespace First
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window // наше окно
     {
-        List<string> products;   
+        List<string> products;   // список товаров
 
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); // инициализация компонентов в окне
             products = new List<string>();
         }
 
 
-        private void FirstButtonClick(object sender, RoutedEventArgs e)
+        private void FirstButtonClick(object sender, RoutedEventArgs e) // кнопка добавления первого товара
         {
-            if (!string.IsNullOrWhiteSpace(firstTextBox.Text))
+            if (!string.IsNullOrWhiteSpace(firstTextBox.Text)) // если название не пустое, то добавить товар
             {
                 products.Add(firstTextBox.Text);
-                firstTextBox.Text = "";
+                firstTextBox.Text = ""; // очистка текстового поля
             }
         }
 
-        private void SecondButtonClick(object sender, RoutedEventArgs e)
+        private void SecondButtonClick(object sender, RoutedEventArgs e) // кнопка добавления второго товара
         {
-            if (!string.IsNullOrWhiteSpace(secondTextBox.Text))
+            if (!string.IsNullOrWhiteSpace(secondTextBox.Text)) // если название не пустое, то добавить товар
             {
                 products.Add(secondTextBox.Text);
-                secondTextBox.Text = "";
+                secondTextBox.Text = ""; // очистка текстового поля
             }
         }
 
-        private void ThirdButtonClick(object sender, RoutedEventArgs e)
+        private void ThirdButtonClick(object sender, RoutedEventArgs e) // кнопка добавления третьего товара
         {
-            if (!string.IsNullOrWhiteSpace(thirdTextBox.Text))
+            if (!string.IsNullOrWhiteSpace(thirdTextBox.Text)) // если название не пустое, то добавить товар
             {
                 products.Add(thirdTextBox.Text);
-                thirdTextBox.Text = "";
+                thirdTextBox.Text = ""; // очистка текстового поля
             }
         }
 
-        private void FourthButtonClick(object sender, RoutedEventArgs e)
+        private void FourthButtonClick(object sender, RoutedEventArgs e) // кнопка добавления четвертого товара
         {
-            if (!string.IsNullOrWhiteSpace(fourthTextBox.Text))
+            if (!string.IsNullOrWhiteSpace(fourthTextBox.Text)) // если название не пустое, то добавить товар
             {
                 products.Add(fourthTextBox.Text);
-                fourthTextBox.Text = "";
+                fourthTextBox.Text = ""; // очистка текстового поля
             }
         }
 
-        private void ButtonClick(object sender, RoutedEventArgs e)
+        private void ButtonClick(object sender, RoutedEventArgs e) // кнопка просмотра корзины
         {
-            string productsString = "";
+            string productsString = ""; // строка продуктов
             foreach (var product in products)
             {
-                productsString += product + ", ";
+                productsString += product + ", "; // из списка продуктов добавляем продукт в строку продуктов через запятую
             }
-            MessageBox.Show(productsString);
+            MessageBox.Show(productsString); // показать список продуктов
         }
     }
 }
